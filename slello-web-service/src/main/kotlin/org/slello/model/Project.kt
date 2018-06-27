f9d64fb49cf9f50b9d7d7a245fcb05a52d22b392
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-06-26 22:37:13 -0300

    Project class

---
+++ slello-web-service/src/main/kotlin/org/slello/model/Project.kt
@@ -0,0 +1,27 @@
+package org.slello.model
+
+import org.bson.types.ObjectId
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+import java.time.LocalDate
+
+@Document
+data class Project(@Id val id: ObjectId,
+                   val name: String,
+                   val status: String,
+                   val description: String,
+                   val startDate: LocalDate,
+                   val endDate: LocalDate,
+                   val tasks: List<Task>)
+
+data class Task(
+        @Id val id: ObjectId,
+        val sprint: Int,
+        val status: Status
+)
+
+enum class Status {
+    IN_PROGRESS,
+    FINISHED,
+    NONE
+}
