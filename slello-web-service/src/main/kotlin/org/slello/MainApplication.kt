8ac0e8f15b233542a6b39a010013fe764a74eb7b
Lucy Narita <13240125+lnarita@users.noreply.github.com>
2018-06-28 12:07:23 -0300

    fix: swagger-ui

--- slello-web-service/src/main/kotlin/org/slello/MainApplication.kt
+++ slello-web-service/src/main/kotlin/org/slello/MainApplication.kt
@@ -3,11 +3,9 @@ package org.slello
 import org.springframework.boot.Banner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 
 
 @SpringBootApplication
-@EnableWebMvc
 class MainApplication
 
 fun main(args: Array<String>) {
