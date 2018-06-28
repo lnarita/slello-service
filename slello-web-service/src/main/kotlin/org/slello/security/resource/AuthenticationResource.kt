ada9937d22b1ce553f35dac5b95106250bcfc61a
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-06-27 23:40:58 -0300

    adding cross origin to auth

--- slello-web-service/src/main/kotlin/org/slello/security/resource/AuthenticationResource.kt
+++ slello-web-service/src/main/kotlin/org/slello/security/resource/AuthenticationResource.kt
@@ -17,6 +17,7 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.authentication.InternalAuthenticationServiceException
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -25,6 +26,7 @@ import javax.servlet.http.HttpServletRequest
 
 
 @RestController
+@CrossOrigin
 class AuthenticationResource @Autowired constructor(val authenticationController: AuthenticationController) {
     companion object {
         const val BASIC_AUTHORIZATION_PREFIX = "Basic "
