ca54ff0b109af15ad906bd468785f2743ff883bf
João Vitor Araki Gonçalves <joao.goncalves@movile.com>
2018-06-27 23:04:16 -0300

    enable cors

---
+++ slello-web-service/src/main/kotlin/org/slello/security/configuration/CorsConfiguration.kt
@@ -0,0 +1,15 @@
+package org.slello.security.configuration
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+@EnableWebMvc
+class CorsConfiguration : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry?) {
+        registry!!.addMapping("/**")
+    }
+}
